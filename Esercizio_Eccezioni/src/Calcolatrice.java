5190a9c21867d478895be37ab0985f4f1b7a633c
simonenapolitano <simone.napolitano.08@marconigorgonzola.it>
2026-01-01 18:24:05 +0100

    Rimosso Esercizio_Eccezioni

--- Esercizio_Eccezioni/src/Calcolatrice.java
+++
@@ -1,8 +0,0 @@
-public class Calcolatrice {
-    static public double dividi(double dividendo, double divisore){
-        return dividendo/divisore;
-    }
-    static public void accediArray(int[] arr, int i){
-        System.out.println("Indice " + i + " dell'array : " + arr[i]);
-    }
-}
