fe6f469a7a476b1c8bd25678ff2c66f9808dc95e
simonenapolitano <simone.napolitano.08@marconigorgonzola.it>
2026-01-22 14:15:20 +0100

    Inizio dell'esercitazione 11 java

---
+++ Negozio/ValoreScontoException.java
@@ -0,0 +1,8 @@
+public class ValoreScontoException extends Exception{
+    public ValoreScontoException(String message){
+        super(message);
+    }
+    public ValoreScontoException(){
+        super("Valore sconto non valido! [1-100]");
+    }
+}
