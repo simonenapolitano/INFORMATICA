60a7c1eef7d9d898dd1aff0a67c3a6df10243ff0
simonenapolitano <simone.napolitano.08@marconigorgonzola.it>
2026-03-12 11:51:48 +0100

    Aggiunta lettura del file

--- scrittura_file_txt/Main.java
+++ scrittura_file_txt/Main.java
@@ -1,18 +1,43 @@
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main{
 
-    public static void main(String[] args) {
-        String percorso = "files/file.txt";
-        try (FileWriter fileWriter = new FileWriter(percorso);
-            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) 
+    public Main(){
+        scriviSuFile("files/file.txt");
+        String contenuto = leggiFile("files/file.txt");
+        System.out.println("Contenuto del file: " + contenuto);
+    }
+
+    private void scriviSuFile(String percorso){
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(percorso));) 
         {
             bufferedWriter.write("Ciao");
             System.out.println("Archiviazione completata");
         } catch (IOException e) {
             System.out.println("ERRORE: " + e.getMessage());
         }
+    }
+
+    private String leggiFile(String percorso){
+        try(BufferedReader reader = new BufferedReader(new FileReader(percorso))) {
+            String linea;
+            String contenuto = "";
+            while((linea = reader.readLine()) != null){
+                contenuto += linea;
+            }
+            return contenuto;
+        } catch (IOException e) {
+            System.out.println("ERRORE: " + e.getMessage());
+            return "ERRORE";
+        }
+    }
+
+    public static void main(String[] args) {
+        new Main();
+        
     }
 }
